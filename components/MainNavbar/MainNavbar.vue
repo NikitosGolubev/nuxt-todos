389a3aa7ea97b225d00ead1d318713e51b969dab
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2020-09-23 09:38:52 +0300

    Made menu displaying it's content depend on user authentication status

--- components/MainNavbar/MainNavbar.vue
+++ components/MainNavbar/MainNavbar.vue
@@ -3,61 +3,65 @@
     <v-app-bar fixed color="deep-purple darken-1" dark>
       <v-container class="d-flex justify-space-between align-center">
         <div>
-          <nuxt-link to="/" class="white--text">
+          <nuxt-link :to="mainPageLink" class="white--text">
             <span class="text-h5 font-weight-medium">Nuxt TODO</span>
           </nuxt-link>
         </div>
 
         <div class="navigation-links">
-          <div class="navigation-links__item">
-            <nuxt-link
-              exact
-              active-class="link_active"
-              class="link link_white"
-              to="/"
-            >
-              Main page
-            </nuxt-link>
-          </div>
-          <div class="navigation-links__item">
-            <nuxt-link to="/login">
-              <v-btn outlined color="white">
-                Sign in
-              </v-btn>
-            </nuxt-link>
-          </div>
-          <div class="navigation-links__item">
-            <nuxt-link to="/register">
-              <v-btn depressed color="deep-orange">
-                Register
-              </v-btn>
-            </nuxt-link>
-          </div>
+          <template v-if="!isAuthenticated">
+            <div class="navigation-links__item">
+              <nuxt-link
+                exact
+                active-class="link_active"
+                class="link link_white"
+                to="/"
+              >
+                Main page
+              </nuxt-link>
+            </div>
+            <div class="navigation-links__item">
+              <nuxt-link to="/login">
+                <v-btn outlined color="white">
+                  Sign in
+                </v-btn>
+              </nuxt-link>
+            </div>
+            <div class="navigation-links__item">
+              <nuxt-link to="/register">
+                <v-btn depressed color="deep-orange">
+                  Register
+                </v-btn>
+              </nuxt-link>
+            </div>
+          </template>
 
-          <div class="navigation-links__item">
-            <nuxt-link
-              exact
-              active-class="link_active"
-              class="link link_white"
-              to="/todos"
-            >
-              My todos
-            </nuxt-link>
-          </div>
-          <div class="navigation-links__item">
-            <nuxt-link to="/todos/create">
-              <v-btn color="green">
-                Create new
-              </v-btn>
-            </nuxt-link>
-          </div>
-          <div class="navigation-links__item">
-            <nuxt-link to="/logout">
-              <v-btn depressed color="deep-orange">
-                Logout
-              </v-btn>
-            </nuxt-link>
-          </div>
+          <template v-if="isAuthenticated">
+            <div class="navigation-links__item">
+              <nuxt-link
+                exact
+                active-class="link_active"
+                class="link link_white"
+                to="/todos"
+              >
+                My todos
+              </nuxt-link>
+            </div>
+            <div class="navigation-links__item">
+              <nuxt-link to="/todos/create">
+                <v-btn color="green">
+                  Create new
+                </v-btn>
+              </nuxt-link>
+            </div>
+            <div class="navigation-links__item">
+              <nuxt-link to="/logout">
+                <v-btn depressed color="deep-orange">
+                  Logout
+                </v-btn>
+              </nuxt-link>
+            </div>
+          </template>
         </div>
       </v-container>
     </v-app-bar>
@@ -67,6 +71,17 @@
 <script>
 export default {
   name: 'MainNavbar',
+  computed: {
+    isAuthenticated() {
+      /** @todo Abstraction! */
+      return !!this.$fireAuth.currentUser;
+    },
+    
+    mainPageLink() {
+      if (this.isAuthenticated) return '/todos';
+      return '/';
+    }
+  },
 }
 </script>
 
